d9c4661978119f2ff5a7379a14984b2973938f5a
andreatheodora <andreatheodora@yahoo.com>
2025-10-05 17:51:18 +0700

    cors

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,20 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { basicApi } from "../services/basicApi";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://lang-ai-beta.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
+  );
   if (req.method == "POST") {
     const { input, words = [], grammars = [], language, level } = req.body;
     try {
