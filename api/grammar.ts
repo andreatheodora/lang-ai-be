9876b9ce4f7e9d56a9a2891ade0c7720130dfd2e
andreatheodora <andreatheodora@yahoo.com>
2025-10-06 23:25:43 +0700

    file names

--- api/grammar.ts
+++ api/grammar.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { grammarApi } from "../services/grammarApi.js";
+import { grammarApi } from "../services/grammarApi.ts";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method == "POST") {
