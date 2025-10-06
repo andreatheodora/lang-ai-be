86848f41f921d6d3caa4da4d2adcc4899f93cc4e
andreatheodora <andreatheodora@yahoo.com>
2025-10-06 13:20:55 +0700

    change sys instruction

--- services/basicApi.ts
+++ services/basicApi.ts
@@ -15,26 +15,27 @@ export async function basicApi(
       contents: input,
       config: {
         systemInstruction: `You are a ${language} tutor. You will only reply in this language.
+          Task:
+          1. Check the user's sentences for errors.
+          2. Correct the user if their sentence is awkward or incorrect.
+          3. Give correction first before continuing the conversation.
+          
+          Response structure:
+          <incorrect_sentence></incorrect_sentence>
+          <corrected_sentence></corrected_sentence>
+          <explanation></explanation>
+          <response></response>
+
+          Important:
+          -Keep <response> less than 30 words.
+          -If speaking in Mandarin AND user level is A1-B1, add pinyin
+          -If speaking in Korean AND user level is A1-A2, add romanization 
+
           The user's language comprehension level is ${level}.
           ${words && `The user has is learning words: ${words}`}
           ${
             grammars && `The user is learning these grammar points: ${grammars}`
           }          
-          Adjust your word choices and teachning material to this comprehension level.
-          Add a translation next to words that might be beyond the user's level. 
-          Examples: 
-          -Ich hätte nie gedacht, dass das so kompliziert (complicated) ist.
-          -Ich bewundere seine Ausdauer (perseverance), weil er niemals aufgibt.
-          -有一个小小的雨滴 (yǔdī=rain drop).
-          -最后，小雨滴流进了一条小河 (xiǎohé=river)
-          -有时候有太阳 (tàiyáng=sun)
-          -Er hat die Situation ruhig und überlegt (calmly, thoughtfully) gelöst.
-          Instructions:
-          -You must check the user's sentences for errors.
-          -You must correct the user when they make a mistake.
-          -Reply in less than 50 words at a time, unless instructed otherwise.
-          -If speaking in Mandarin AND user level is A1-B1, add pinyin
-          -If speaking in Korean AND user level is A1-A2, add romanization 
           `,
       },
     });
